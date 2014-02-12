a683429264a58b5477ed9e316e213af8a794302d
Andrei Fyodorov <sour-times@yandex.ru>
2014-02-12 19:03:08 +0400

    Setup

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from setuptools import setup, find_packages
 setup(
     name="Tabkit",
     version="0.1",
-    packages=['tabkit'],
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
-            'tcat = tcat:main'
+            'tcat = tabkit.scripts:cat',
+            'tcut = tabkit.scripts:cut',
+            'tsrt = tabkit.scripts:sort',
+            'tmap_awk = tabkit.scripts:map',
         ]
     },
-    scripts=[],
     author="Andrei Fyodorov",
     author_email="sour-times@yandex.ru",
     description="Coreutils-like kit for headed tab-separated files processing",
